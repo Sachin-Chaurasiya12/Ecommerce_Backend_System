ce67fbeea2760d72322ecb3a4e172fe8408e4a6d
Sachin Chaurasiya <sachinchau444@gmail.com>
2026-03-15 16:30:24 +0530

    Revert "Order model, Essentials Created"
    This reverts commit 63d524dc64beb867034b5fded16540ce6c908f57.

--- src/main/java/com/example/EcommBackend/dto/OrderRequest.java
+++ src/main/java/com/example/EcommBackend/dto/OrderRequest.java
@@ -1,31 +1,34 @@
 package com.example.EcommBackend.dto;
 
-import java.util.List;
-
 public class OrderRequest {
+    
     private Long userId;
     private String shippingAddress;
-    private List<OrderItemDTO> items;
+    private String payment;
 
     public OrderRequest(){}
-
-    public List<OrderItemDTO> getItems() {
-        return items;
-    }
-    public void setItems(List<OrderItemDTO> items) {
-        this.items = items;
-    }
-    public String getShippingAddress() {
-        return shippingAddress;
-    }
-    public void setShippingAddress(String shippingAddress) {
+    public OrderRequest(Long userId,String shippingAddress,String payment){
+        this.userId = userId;
         this.shippingAddress = shippingAddress;
+        this.payment = payment;
     }
+
     public Long getUserId() {
         return userId;
     }
     public void setUserId(Long userId) {
         this.userId = userId;
     }
-
+    public String getPayment() {
+        return payment;
+    }
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+    public String getShippingaddress() {
+        return shippingAddress;
+    }
+    public void setShippingaddress(String shippingaddress) {
+        this.shippingAddress = shippingaddress;
+    }
 }
--- src/main/java/com/example/EcommBackend/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EcommBackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.EcommBackend.model.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders,Long>{
-    
-}
